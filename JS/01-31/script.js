af60ebc905c16cf94c3d20b7d181f72202579ace
mantas <nacevicius@gmail.com>
2023-01-31 18:17:38 +0200

    “js”

---
+++ JS/01-31/script.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 50;
+
+let addition = x + y;
+let substraction = y - x;
+let multiplication = y*x;
+let division = y/x;
+
+let answer = `addition: ${addition} \n substraction: ${substraction} \n multiplication: ${multiplication} \n division: ${division}\n`
+console.log(answer);
